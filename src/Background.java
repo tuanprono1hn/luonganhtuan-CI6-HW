b1af11442a453ec364a567b1f1876d95317b505a
tuanprono1hn <a6quocoai@gmail.com>
2018-06-06 00:05:55 +0700

    UDHW

--- src/Background.java
+++ src/Background.java
@@ -1,15 +1,13 @@
 import java.awt.*;
 
-public class Background {
-    public Vector2D position;
-    public Renderer renderer;
+public class Background extends GameObject{
 
     public Background() {
         this.position = new Vector2D();
         this.renderer = new BackgroundRenderer(Color.black,1024,600);
     }
 
-    public void render(Graphics graphics){
-        this.renderer.render(graphics,this.position);
-    }
+//    public void render(Graphics graphics){
+//        this.renderer.render(graphics,this.position);
+//    }
 }
--- src/PlayerShoot.java
+++ src/PlayerShoot.java
@@ -3,26 +3,26 @@ import java.util.List;
 
 public class PlayerShoot {
 
-    private int bullet = 0;
+    private FrameCounter frameCounter;
 
     public List<Bullet> bulletPlayers;
 
     public PlayerShoot() {
         this.bulletPlayers = new ArrayList<>();
+        this.frameCounter = new FrameCounter(10);
     }
 
     public void run(Player player){
         Bullet bulletPlayer = new Bullet();
-        if (this.bullet == 20){
+        if (this.frameCounter.run()){
             bulletPlayer.position.set(player.position);
 
             Vector2D rotate = player.playerMove.velocity.add(new Vector2D(2,0).rotate(player.playerMove.angle));
 
             bulletPlayer.velocity.set(rotate);
             this.bulletPlayers.add(bulletPlayer);
-            this.bullet = 0;
-        } else {
-            bullet += 1;
+            this.frameCounter.reset();
         }
+        this.bulletPlayers.forEach(bullet -> bullet.run());
     }
 }
